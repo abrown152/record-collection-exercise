cdcdb27aad12e343974c8db767d286f3ffe9a93c
Alysia Brown <alysiacbrown@gmail.com>
2016-04-11 16:20:11 -0700

    Added reset_schema! method for deleting the database.

--- lib/setup_db.rb
+++ lib/setup_db.rb
@@ -5,9 +5,34 @@ class ReleaseDatabase
 
   def initialize(dbname = "releases")
     @db = SQLite3::Database.new("database/#{dbname}.db")
-
   end
+
+  def reset_schema!
+    query = <<-CREATESTATEMENT
+      CREATE TABLE albums (
+        id INTEGER PRIMARY KEY,
+        label_code BLOB,
+        artist TEXT NOT NULL,
+        title TEXT NOT NULL,
+        label TEXT,
+        format TEXT,
+        released INTEGER,
+        date_added TEXT
+      );
+    CREATESTATEMENT
+    # lines 11-21 are one giant string
+
+    @db.execute("DROP TABLE IF EXISTS albums;")
+    @db.execute(query) # runs one and only one query
+
+    # label_code,artist,title,label,format,released,date_added
+    #BLOB, TEXT, TEXT, TEXT, FORMAT, INTEGER, TEXT
+    # table name: albums
+    # one table
+  end
+
 
 end
 
 release_db = ReleaseDatabase.new
+release_db.reset_schema!
